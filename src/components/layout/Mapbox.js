eabd7ba9cf3f962c7dac1c0f1d609a60eb27a04a
David Sellam <dvdsellam@gmail.com>
2020-02-08 12:53:02 +0200

    map env

--- src/components/layout/Mapbox.js
+++ src/components/layout/Mapbox.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactMapboxGl from "react-mapbox-gl";
 
 const Mapbox = () => {
-  const mapStyle = "mapbox://styles/dvd90/ck6dgjim00pgp1isan89qwfig";
+  const mapStyle = { link: "mapbox://styles/dvd90/ck6dgjim00pgp1isan89qwfig" };
   const Map = ReactMapboxGl({
-    accessToken:
-      "pk.eyJ1IjoiZHZkOTAiLCJhIjoiY2s1NzhnMTYwMGF2ZDNncGFiam1vaG96bCJ9.ihLfpKCfY6EwLtAt9AoEnw"
+    accessToken: process.env.REACT_APP_MAP_BOX_KEY
   });
 
   return (
     <Map
-      style={mapStyle}
+      style={mapStyle.link}
       containerStyle={{
         height: "100vh",
         width: "100vw"
